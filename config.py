777327ae869028dc01b318ae0c304472445dbd2e
saa14850-del <saa14850@gmail.com>
2025-09-16 11:02:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Local development only
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ADMINS = [7891208589]  # এখানে তোমার  আইডি 
+ADMINS = [int(os.environ.get("ADMIN_ID"))]
+GROUP_ID = int(os.environ.get("GROUP_ID"))
