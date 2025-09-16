d64874c2396a81f4ee35f8449351c7bcc7360453
saa14850-del <saa14850@gmail.com>
2025-09-16 12:04:24 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()  # Local development only
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMINS = [int(os.environ.get("ADMIN_ID"))]
 
-GROUP_ID = os.environ.get("GROUP_ID")  # string-compatible, numeric ID বা link/username
-if not GROUP_ID:
-    raise ValueError("GROUP_ID environment variable set নেই!")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN environment variable set নেই!")
+if not ADMINS:
+    raise ValueError("ADMIN_ID environment variable set নেই!")
